cb92b3357ae27850ec07cba24a6192b15559f5e5
Yury Kazakevich <y.whitest@gmail.com>
2019-08-16 23:56:56 +0300

    Upgraded sync.py

--- Yury_Kazakevich/03.GIT.Hosting/sync.py
+++ Yury_Kazakevich/03.GIT.Hosting/sync.py
@@ -3,18 +3,32 @@ import os
 import sys
 import re
 
-def git_push(PATH_OF_GIT_REPO, REMOTE):
+'''
+This scrips pushes current branch to all remote repositories except "prepod"
+How-to:
+1. Go to you git working directory (where .git folder is being located)
+2. Run the script: python sync.py
+3. Provide comment message.
+'''
+
+PATH_OF_GIT_REPO = r'.git'  # make sure .git folder is properly configured
+COMMIT_MESSAGE = input("Specify a comment please: ")
+COMMIT_MESSAGE = str(COMMIT_MESSAGE).strip()
+CFG = f"{PATH_OF_GIT_REPO}/config"
+repo = Repo(PATH_OF_GIT_REPO)
+
+def git_push(PATH_OF_GIT_REPO, REMOTE, COMMIT_MESSAGE):
     try:
         repo = Repo(PATH_OF_GIT_REPO)
-        #repo.git.add(update=True)
+        repo.git.add(update=True)
         #repo.git.add()
-        origin = repo.remote(name=(str(REMOTE)))
+        repo.index.commit(COMMIT_MESSAGE)
+        origin = repo.remote(name=REMOTE)
         #origin.fetch()
         origin.push(force=True)
-        print('Successfull push to: ', REMOTE)
+        print('Successfully pushed to: ', REMOTE)
     except Exception as e:
         print(f'Some error occured while pushing the code to {REMOTE}: ',e)
-
 
 def readcfg(CFG):
     # get upstreams from config
@@ -31,19 +45,14 @@ def readcfg(CFG):
                     res.append(match[0][2])
                 c += 1
     else:
-        print("Config not found! Change you apth to git working directory and try again. Exiting...")
+        print("Config not found! Change you path to git working directory and try again. Exiting...")
         sys.exit(1)
     return res
 
-
-PATH_OF_GIT_REPO = r'.git'  # make sure .git folder is properly configured
-COMMIT_MESSAGE = input("Specify a comment please: ")
-CFG = f"{PATH_OF_GIT_REPO}/config"
-    #git_push()
 w = readcfg(CFG)
-repo = Repo(PATH_OF_GIT_REPO)
-repo.index.commit(str(COMMIT_MESSAGE).strip())
 
 for REMOTE in w:
+    REMOTE = str(REMOTE).strip()
     if REMOTE != 'prepod':
-        git_push(PATH_OF_GIT_REPO, REMOTE)
+        git_push(PATH_OF_GIT_REPO, REMOTE, COMMIT_MESSAGE)
+
